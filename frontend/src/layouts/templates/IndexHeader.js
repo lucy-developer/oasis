ec6bc7916722e31b253b625271f1eb5d5958fe12
lia318 <lia318@secnc.co.kr>
2020-03-24 11:42:01 +0900

    # 로그인 페이지 UI - CSS 수정

--- frontend/src/layouts/templates/IndexHeader.js
+++ frontend/src/layouts/templates/IndexHeader.js
@@ -4,11 +4,25 @@ import logo from '../../assets/img/oasis_logo.png';
 
 class IndexHeader extends React.Component {
     render() {
+        const indexNav = {
+            width: "30%",
+            height: "100vh",
+            padding: "0",
+            margin: "0px",
+            paddingBottom: "99px",
+            border: "0px"
+        };
+
+        const indexNavDiv = {
+            height: "100%"
+            /*backgroundColor: "#f3f6f8"*/
+        };
+
         return (
             <>
-                <Navbar className="index-header navbar-absolute fixed-top" expand="lg" color="transparent">
+                <Navbar style={indexNav} className="index-header navbar-absolute fixed-top" expand="lg" color="transparent">
                     <Container>
-                        <div>
+                        <div style={indexNavDiv}>
                             <NavbarBrand href="/">
                                 <img width="160" src={logo} alt="logo" />
                             </NavbarBrand>
--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -81,57 +81,107 @@ class Login extends React.Component {
 
     render() {
         const { signInStore } = this.props;
+
+        const loginForm = {
+            width: "536px"
+        };
+
+        const loginInputGroup = {
+            paddingBottom: "20px"
+        };
+
+        const loginInput = {
+            height: "60px",
+            borderRadius: "4px",
+            boxShadow: "0px 2px 2px 0 rgba(76, 81, 83, 0.05)",
+            border: "solid 1px #e0e0e0",
+
+            fontFamily: "NotoSansCJKkr",
+            fontSize: "22px",
+            fontWeight: "normal",
+            fontStretch: "normal",
+            fontStyle: "normal",
+            letterSpacing: "-0.44px",
+            color: "#000000"
+        };
+
+        const loginBtnFooter = {
+            padding: "22px 15px"
+        };
+
+        const loginBtn = {
+            margin: "0px",
+            height: "80px",
+            borderRadius: "4px",
+            backgroundColor: "#1e7fba",
+
+            fontFamily: "NotoSansCJKkr",
+            fontSize: "26px",
+            fontWeight: "normal",
+            fontStretch: "normal",
+            fontStyle: "normal",
+            letterSpacing: "-0.52px",
+            color: "#ffffff"
+        };
+
         return (
             <div className="login-page">
                 <Container>
-                    <Row>
+                    <Row style={{ paddingLeft: "76px" }}>
                         <Col className="ml-auto mr-auto" lg="4" md="6">
-                            <Form action="" className="form" method="">
+                            <Form action="" className="form" method="" style={loginForm}>
                                 <Card className="card-login">
                                     <CardHeader>
                                         <CardHeader>
                                             <div className="text-center login-header">
-                                                <img className="custom-logo" src={logo} alt="logo" />
+                                                {/*<img className="custom-logo" src={logo} alt="logo" />*/}
+                                                Login
                                             </div>
                                         </CardHeader>
                                     </CardHeader>
                                     <CardBody>
-                                        <InputGroup>
+                                        <InputGroup style={loginInputGroup}>
+                                            {/*
                                             <InputGroupAddon addonType="prepend">
                                                 <InputGroupText>
                                                     <i className="nc-icon nc-single-02 text-danger" />
                                                 </InputGroupText>
                                             </InputGroupAddon>
+                                            */}
                                             <Input
-                                                placeholder="휴대폰번호 또는 ID"
+                                                placeholder="E-mail"
                                                 type="text"
                                                 autoComplete="off"
                                                 name="loginName"
                                                 onChange={signInStore.handleChange}
                                                 onKeyPress={this.keyPressInputId}
+                                                style={loginInput}
                                             />
                                         </InputGroup>
                                         <InputGroup>
+                                            {/*
                                             <InputGroupAddon addonType="prepend">
                                                 <InputGroupText>
                                                     <i className="nc-icon nc-key-25 text-danger" />
                                                 </InputGroupText>
                                             </InputGroupAddon>
+                                            */}
                                             <Input
-                                                placeholder="비밀번호"
+                                                placeholder="PW"
                                                 type="password"
                                                 autoComplete="off"
                                                 name="password"
                                                 onChange={signInStore.handleChange}
                                                 onKeyPress={this.keyPressPassword}
+                                                style={loginInput}
                                             />
                                         </InputGroup>
                                         <Badge className="errorBadge" color="danger">
                                             {signInStore.errorMessage}
                                         </Badge>
                                     </CardBody>
-                                    <CardFooter>
-                                        <Button block className="btn-default mb-3" color="default" onClick={this.login}>
+                                    <CardFooter style={loginBtnFooter}>
+                                        <Button block className="btn-default mb-3" color="default" style={loginBtn} onClick={this.login}>
                                             로그인
                                         </Button>
                                     </CardFooter>
